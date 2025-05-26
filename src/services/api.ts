d12a7e3b80bd55a99362ecab6c3797c69c7a352e
Juan José Díaz <81777362+Juan-Jose-D@users.noreply.github.com>
2025-05-26 01:08:07 -0500

    cambio en las url

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,8 +5,8 @@ const isLocalhost = window.location.hostname === 'localhost' ||
     window.location.hostname === '127.0.0.1';
 
 const baseURL = isLocalhost
-    ? 'http://localhost:8000'
-    : 'https://desplieguebackproyecto-caewexbzb2hbhje2.eastus-01.azurewebsites.net';
+    ? 'https://desplieguebackproyecto-caewexbzb2hbhje2.eastus-01.azurewebsites.net'
+    : 'http://localhost:8000';
     
 
 const api = axios.create({
